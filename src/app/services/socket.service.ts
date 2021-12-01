c6d520c69b61148d8eb12c8ace42826bf2b35f11
Nada <madadnada@gmail.com>
2021-12-01 14:36:31 +0100

    fixing socket bug

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -40,6 +40,6 @@ export class SocketService implements OnDestroy {
   }
 
   showToaster(data: Notification) {
-    this._toasterService.show(data.title, data.content);
+    this._toasterService.show(data);
   }
 }
